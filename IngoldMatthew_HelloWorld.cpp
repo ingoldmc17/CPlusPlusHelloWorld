869347fd5eea41557ec24a47820c67d8f8ee6b62
Matt Ingold <ingoldmc17@gmail.com>
2017-08-23 21:18:57 -0400

    First commit

---
+++ IngoldMatthew_HelloWorld.cpp
@@ -0,0 +1,17 @@
+//Chapter 1, Testing IDE
+//Filename: HelloWorld.cpp
+//Programmer: Matt Ingold
+//Date: 1/18/17
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	cout << "Hello world" << endl;
+
+	system("pause");
+	return 0;
+
+}
